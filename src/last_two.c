c4e04765742baac24645f18e77dad9b9267f944a
Christian Deacon <christian@moddingcommunity.com>
2023-10-05 23:04:47 +0000

    More organization.

--- src/last_two.c
+++ src/last_two.c
@@ -9,13 +9,13 @@
 #include "common.h"
 
 SEC("xdp_prog")
-int prog(struct xdp_md *ctx) {
+int prog(struct xdp_md* ctx) {
     // Initialize data and data end.
     void* data = (void*)(long)ctx->data;
     void* data_end = (void*)(long)ctx->data_end;
 
     // Initialite IP header and check.
-    struct iphdr *iph = data + sizeof(struct ethhdr);
+    struct iphdr* iph = data + sizeof(struct ethhdr);
 
     if (iph + 1 > (struct iphdr *)data_end)
         return XDP_DROP;
@@ -25,7 +25,7 @@ int prog(struct xdp_md *ctx) {
         return XDP_PASS;
 
     // Initialize TCP header and check.
-    struct tcphdr *tcph = (struct tcphdr *)iph + 1;
+    struct tcphdr* tcph = (struct tcphdr*)iph + 1;
 
     if (tcph + 1 > (struct tcphdr *)data_end)
         return XDP_DROP;
@@ -34,18 +34,18 @@ int prog(struct xdp_md *ctx) {
     __u16 ipLen = ntohs(iph->tot_len);
 
     // Initialize payload.
-    __u8 *pl = (__u8 *)(tcph + 1);
+    __u8* pl = (__u8*)(tcph + 1);
     
     // Retrieve offset to last packet.
     __u16 off = ipLen - (iph->ihl * 4) - (tcph->doff * 4);
 
     // Initialize last byte of data and check.
-    __u8 *last = pl + off;
+    __u8* last = pl + off;
 
-    if (last + 1 > (__u8 *)data_end)
+    if (last + 1 > (__u8*)data_end)
         return XDP_PASS;
 
-    if (last < (__u8 *)data)
+    if (last < (__u8*)data)
         return XDP_PASS;
 
     // Print the last byte of data to /sys/kernel/tracing/trace_pipe.
